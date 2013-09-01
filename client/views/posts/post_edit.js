347dfd4567a1da30c2e0dd3eeea53672e61b1d63
Eric Pan <ericpan@gmail.com>
2013-08-31 20:47:02 -0700

    Commit 8-1 Added edit posts form

---
+++ client/views/posts/post_edit.js
@@ -0,0 +1,37 @@
+Template.postEdit.helpers({
+  post: function() {
+    return Posts.findOne(Session.get('currentPostId'));
+  }
+});
+
+Template.postEdit.events({
+  'submit form': function(event) {
+    event.preventDefault();
+
+    var currentPostId = Session.get('currentPostId');
+
+    var postProperties = {
+      url: $(event.target).find('[name=url]').val(),
+      title: $(event.target).find('[name=title]').val()
+    };
+
+    Posts.update(currentPostId, {$set: postProperties}, function(error) {
+      if (error) {
+        // display the error to the user
+        alert(error.reason);
+      } else {
+        Meteor.Router.to('postPage', currentPostId);
+      }
+    });
+  },
+
+  'click .delete': function(event) {
+    event.preventDefault();
+
+    if (confirm("Delete this post?")) {
+      var currentPostId = Session.get('currentPostId');
+      Posts.remove(currentPostId);
+      Meteor.Router.to('postsList');
+    }
+  }
+});
