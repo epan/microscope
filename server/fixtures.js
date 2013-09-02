b56ac095e1e38e50f1adb31f737192a3581c0268
Eric Pan <ericpan@gmail.com>
2013-09-02 16:07:15 -0700

    Commit 10-1 Added comments collection, pub/sub and fixtures.

--- server/fixtures.js
+++ server/fixtures.js
@@ -1,4 +1,42 @@
+// Fixture data
 if (Posts.find().count() === 0) {
+  var now = new Date().getTime();
+
+  // create two users
+  var slinId = Meteor.users.insert({
+    profile: {name: 'Slin Lee'}
+  });
+  var slin = Meteor.users.findOne(slinId);
+
+  var kellyId = Meteor.users.insert({
+    profile: {name: 'Kelly Miyashiro'}
+  });
+  var kelly = Meteor.users.findOne(kellyId);
+
+  var kellyblogId = Posts.insert({
+    title: 'Kelly Miyashiro - UX Design',
+    userId: kelly._id,
+    author: kelly.profile.name,
+    url: 'http://kellymiyashiro.com/',
+    submitted: now - 7 * 3600 * 1000
+  });
+
+  Comments.insert({
+    postId: kellyblogId,
+    userId: slin._id,
+    author: slin.profile.name,
+    submitted: now - 5 * 3600 * 1000,
+    body: 'Interesting project Kelly, can I get involved?'
+  });
+
+  Comments.insert({
+    postId: kellyblogId,
+    userId: kelly._id,
+    author: kelly.profile.name,
+    submitted: now - 3 * 3600 * 1000,
+    body: 'Sure you can, Slin!'
+  });
+
   Posts.insert({
     title: 'Introducing Telescope',
     author: 'Sacha Grief',
@@ -17,3 +55,4 @@ if (Posts.find().count() === 0) {
     url: 'http://themeteorbook.com'
   });
 }
+
